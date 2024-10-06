b8942640fe72377d343999a85b8a3725be279831
Temutjin <tamutdzhin2006@mail.ru>
2024-10-06 19:53:32 +0500

    Intro

--- src/main.java
+++ src/main.java
@@ -1,5 +1,24 @@
 public class main {
+    public interface Creature{
+        void speak();
+    }
+
+    public static class Human implements Creature{
+        private String name;
+        private String group;
+        public Human(String name, String group){
+            this.name = name;
+            this.group = group;
+        }
+
+        public void speak(){
+            System.out.println("Hello my name is " + this.name);
+            System.out.println("I'm from group " + this.group);
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Salem");
+        Creature temutjin = new Human("Temutjin Koszhanov", "SE-2308");
+        temutjin.speak();
     }
 }
